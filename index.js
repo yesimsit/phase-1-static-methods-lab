3f869aa5b74cfadc8f740ca2debe371071ae6d0b
yesim <sityesim@gmail.com>
2022-06-10 10:08:30 +0300

    completed

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  
+     static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+ 
+  
+    static sanitize(string) {
+      return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+   }
+ 
+  
+    static titleize(string) {
+  let filter=
+  [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+  let result=[]
+  let array=string.split(" ")
+  for(let i=0; i<array.length; i++){
+    if(i === 0){
+      result.push(this.capitalize(array[i]))
+    }else{
+      if(filter.includes(array[i])){
+        result.push(array[i])
+      }else{
+        result.push( this.capitalize( array[i]))
+      }
+      }
+    
+  }
+
+  return result.join( " " );
+ }
+
 }
